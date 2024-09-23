50f901e3347f2cbcf8d3e4428beb82b6edfc6d3a
한효찬 <hanhyochan@hanhyochan-ui-MacBookPro.local>
2024-09-23 08:40:19 -0700

    09/24

--- 10회차/script.jsx
+++ 10회차/script.jsx
@@ -3,7 +3,9 @@ const addBtn = document.querySelector('#addBtn');
 const removeBtn = document.querySelector('#removeBtn');
 const num = document.querySelector('.h_num');
 const title = document.querySelector('.title')
-const popup = document.querySelector('popUp')
+const popup = document.querySelector('.popUp')
+const yes_btn = document.querySelector('.yes_btn')
+const no_btn = document.querySelector('.no_btn')
 
 const hamburgers = ['불고기버거', '치킨버거', '치즈버거', '새우버거', '코딩버거', '카레버거', '콩고기버거', '야채버거']
 
@@ -46,12 +48,23 @@ function popUpOn() {
     console.log(popup.classList.contains('show'));
 }
 
+function yes_btn2() {
+    popup.classList.toggle('show');
+    location.reload();
+}
+
+function no_btn2() {
+    popup.classList.toggle('show');
+}
+
 console.log(new_ham_Array.length)
 console.log(new_ham_Array)
 
 addBtn.addEventListener('click', addItem);
 removeBtn.addEventListener('click', removeItem);
 title.addEventListener('click', popUpOn);
+yes_btn.addEventListener('click', yes_btn2);
+no_btn.addEventListener('click', no_btn2);
 
 //햄버거에 있는 배열을 가져와서 랜덤한 인덱스 값으로 다른 배열에서 복사해서 하나씩 추가하기..
 // 그 배열이 각각 string이 될 줄이야 이걸 쌓아서 배열로 만들어야 하는디
