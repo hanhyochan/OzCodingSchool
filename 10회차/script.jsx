a39f1e25641fc1461aaa4070f0f117289b4bebd4
한효찬 <hanhyochan@hanhyochan-ui-MacBookPro.local>
2024-09-23 09:26:18 -0700

    09/24

--- 10회차/script.jsx
+++ 10회차/script.jsx
@@ -1,21 +1,21 @@
 const itemList = document.querySelector('ul');
 const addBtn = document.querySelector('#addBtn');
 const removeBtn = document.querySelector('#removeBtn');
-const num = document.querySelector('.h_num');
+const Hamburger_counting = document.querySelector('.Hamburger_counting');
 const title = document.querySelector('.title')
 const popup = document.querySelector('.popUp')
-const yes_btn = document.querySelector('.yes_btn')
-const no_btn = document.querySelector('.no_btn')
+const popup_yes = document.querySelector('.popup_yes')
+const popup_no = document.querySelector('.popup_no')
 
 const hamburgers = ['불고기버거', '치킨버거', '치즈버거', '새우버거', '코딩버거', '카레버거', '콩고기버거', '야채버거']
 
-const new_ham_Array = [];
+const new_hamburger_Array = [];
 
-function count_ham() {``
-    if (new_ham_Array.length === 0) {
+function count_hamburger() {``
+    if (new_hamburger_Array.length === 0) {
         num.textContent = ``
     } else {
-        num.textContent = `${new_ham_Array.length}개의 버거를 추가하셨습니다.`
+        Hamburger_counting.textContent = `${new_hamburger_Array.length}개의 버거를 추가하셨습니다.`
     }
 }
 
@@ -23,24 +23,24 @@ function addItem() {
     const li = document.createElement('li')
 
     let rn = Math.random();
-    let rn_ham = Math.floor(rn * hamburgers.length)
-    let new_ham = hamburgers[rn_ham]
+    let rn_hamburger = Math.floor(rn * hamburgers.length)
+    let new_hamburgers = hamburgers[rn_hamburger]
 
-    new_ham_Array.push(new_ham)
+    new_hamburger_Array.push(new_hamburgers)
     
-    li.innerHTML = `${new_ham}`
+    li.innerHTML = `${new_hamburgers}`
     itemList.appendChild(li)
-    count_ham()
+    count_hamburger()
 }
 
 function removeItem() {
-    new_ham_Array.pop()  
+    new_hamburger_Array.pop()  
 
     const liElement = itemList.getElementsByTagName('li')
     if (liElement.length > 0) {
         itemList.removeChild(liElement[liElement.length -1])
     }
-    count_ham()
+    count_hamburger()
 }
 
 function popUpOn() {
@@ -48,23 +48,20 @@ function popUpOn() {
     console.log(popup.classList.contains('show'));
 }
 
-function yes_btn2() {
-    popup.classList.toggle('show');
+function popup_yes_click() {
+    popup.classList.add('show');
     location.reload();
 }
 
-function no_btn2() {
-    popup.classList.toggle('show');
+function popup_no_click() {
+    popup.classList.add('show');
 }
-
-console.log(new_ham_Array.length)
-console.log(new_ham_Array)
 
 addBtn.addEventListener('click', addItem);
 removeBtn.addEventListener('click', removeItem);
 title.addEventListener('click', popUpOn);
-yes_btn.addEventListener('click', yes_btn2);
-no_btn.addEventListener('click', no_btn2);
+popup_yes.addEventListener('click', popup_yes_click);
+popup_no.addEventListener('click', popup_no_click);
 
 //햄버거에 있는 배열을 가져와서 랜덤한 인덱스 값으로 다른 배열에서 복사해서 하나씩 추가하기..
 // 그 배열이 각각 string이 될 줄이야 이걸 쌓아서 배열로 만들어야 하는디
