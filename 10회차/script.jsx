1fa38becda336d7c5c6b0f51a3abca887c96b9dc
한효찬 <hanhyochan@hanhyochan-ui-MacBookPro.local>
2024-09-23 08:01:39 -0700

    09/23

--- 10회차/script.jsx
+++ 10회차/script.jsx
@@ -1,11 +1,21 @@
 const itemList = document.querySelector('ul');
 const addBtn = document.querySelector('#addBtn');
 const removeBtn = document.querySelector('#removeBtn');
-const num = document.querySelector('h_num');
+const num = document.querySelector('.h_num');
+const title = document.querySelector('.title')
+const popup = document.querySelector('popUp')
 
 const hamburgers = ['불고기버거', '치킨버거', '치즈버거', '새우버거', '코딩버거', '카레버거', '콩고기버거', '야채버거']
 
 const new_ham_Array = [];
+
+function count_ham() {``
+    if (new_ham_Array.length === 0) {
+        num.textContent = ``
+    } else {
+        num.textContent = `${new_ham_Array.length}개의 버거를 추가하셨습니다.`
+    }
+}
 
 function addItem() {
     const li = document.createElement('li')
@@ -15,18 +25,33 @@ function addItem() {
     let new_ham = hamburgers[rn_ham]
 
     new_ham_Array.push(new_ham)
-    console.log(new_ham_Array)
-
+    
     li.innerHTML = `${new_ham}`
     itemList.appendChild(li)
+    count_ham()
 }
 
 function removeItem() {
-    new_ham_Array.pop()
+    new_ham_Array.pop()  
+
+    const liElement = itemList.getElementsByTagName('li')
+    if (liElement.length > 0) {
+        itemList.removeChild(liElement[liElement.length -1])
+    }
+    count_ham()
 }
+
+function popUpOn() {
+    popup.classList.toggle('show');
+    console.log(popup.classList.contains('show'));
+}
+
+console.log(new_ham_Array.length)
+console.log(new_ham_Array)
 
 addBtn.addEventListener('click', addItem);
 removeBtn.addEventListener('click', removeItem);
+title.addEventListener('click', popUpOn);
 
 //햄버거에 있는 배열을 가져와서 랜덤한 인덱스 값으로 다른 배열에서 복사해서 하나씩 추가하기..
 // 그 배열이 각각 string이 될 줄이야 이걸 쌓아서 배열로 만들어야 하는디
