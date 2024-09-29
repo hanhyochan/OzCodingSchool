20b3851f23da8dc069c17ccbf644d747d1753732
한효찬 <hanhyochan@hanhyochan-ui-MacBookPro.local>
2024-09-29 07:28:00 -0700

    09/28

--- 계산기/script.jsx
+++ 계산기/script.jsx
@@ -2,23 +2,20 @@ const btns = document.querySelectorAll("button");
 const num_btns = document.querySelectorAll(".number");
 const fuc_btns = document.querySelectorAll(".function");
 const dot_btn = document.querySelector(".dot");
+const operator_btns = document.querySelectorAll(".operator");
 const display_num = document.getElementById("display_num");
 
 let output_num = [0];
 display_num.textContent = output_num;
 
-btns.forEach((x) => {
-  x.addEventListener("click", function (e) {
-    console.log(e.target.textContent);
-  });
-});
-
+// 숫자
 num_btns.forEach((x) => {
   x.addEventListener("click", function (e) {
-    if (output_num[0] === 0) {
-      output_num.shift(0);
+    if (output_num[0] === 0 && output_num.length === 1) {
+      output_num[0] = e.target.textContent;
+    } else if (output_num[0] !== 0) {
       output_num.push(e.target.textContent);
-    } else {
+    } else if (output_num[1] === `.`) {
       output_num.push(e.target.textContent);
     }
 
@@ -26,6 +23,7 @@ num_btns.forEach((x) => {
   });
 });
 
+// 기능
 fuc_btns.forEach((x) => {
   x.addEventListener("click", function (e) {
     if (e.target.textContent === "C") {
@@ -36,13 +34,21 @@ fuc_btns.forEach((x) => {
   });
 });
 
-dot_btn.addEventListener("click", () => {
-  output_num.push(dot_btn.textContent);
-  if (output_num[output_num.length] === ".") {
-    console.log(2);
-  } else {
-    display_num.textContent = output_num.join(``);
-  }
+// 연산
+operator_btns.forEach((x) => {
+  x.addEventListener("click", function (e) {
+
+    if (e.target.textContent === "/") {
+      
+    }
+
+  });
 });
 
-// 소수점숫자를 못친다. 소수점 연속으로 찎힌다ㅠ
+// 점
+dot_btn.addEventListener("click", () => {
+  if (output_num[output_num.length - 1] !== "." && !output_num.includes(".")) {
+    output_num.push(dot_btn.textContent);
+  }
+  display_num.textContent = output_num.join(``);
+});
