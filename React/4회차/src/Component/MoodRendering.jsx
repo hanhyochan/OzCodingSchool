98c1ce5c0a235848457cca62196074eb8d554494
hanhyochan <hcartist569@gmail.com>
2024-10-14 11:56:46 +0900

    10/14

--- React/4회차/src/Component/MoodRendering.jsx
+++ React/4회차/src/Component/MoodRendering.jsx
@@ -1,13 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "../App.css";
 
 const Moodrendering = () => {
-    const [mood, setMood] = useState('gkk')
+  const [mood, setMood] = useState("normal");
+
+  // 1. if문으로 조건부 렌더링
+  //   const MoodEmoji = () => {
+  //     if (mood === 'happy') {
+  //         return <h1>😊</h1>
+  //     } else if (mood === 'normal') {
+  //         return <h1>😑</h1>
+  //     } else {
+  //         return <h1>😢</h1>
+  //     }
+  //   }
+
+  // 2. 삼항연산자로 조건부렌더링
+  //   const MoodEmoji = () => {
+  //     return (
+  //       <>
+  //         {mood === "happy" ? (<h1>😊</h1>
+  //         ) : mood === "normal" ? (
+  //           <h1>😑</h1>
+  //         ) : (
+  //           <h1>😢</h1>
+  //         )}
+  //       </>
+  //     );
+  //   };
+
+  // 3. 논리연산자로 조건부렌더링
+  const MoodEmoji = () => {
     return (
-        <div>
-            <span>{mood}</span>
-            <button>happy</button>
-        </div>
+      <>
+        {mood === "happy" && <h1>😊</h1>}
+        {mood === "normal" && <h1>😑</h1>}
+        {mood === "sad" && <h1>😢</h1>}
+      </>
     );
+  };
+
+  function MoodBtn() {
+    return (
+      <>
+        <button onClick={() => setMood("happy")}>happy</button>
+        <button onClick={() => setMood("normal")}>normal</button>
+        <button onClick={() => setMood("sad")}>sad</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <MoodEmoji />
+      <span
+        className={
+          mood === "happy"
+            ? "happy"
+            : mood === "normal"
+            ? "normal"
+            : mood === "sad"
+        }
+      >
+        mood : {mood}
+      </span>
+      <div>
+        <MoodBtn />
+      </div>
+    </>
+  );
 };
 
 export default Moodrendering;
--- React/4회차/src/main.jsx
+++ React/4회차/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import Moodrendering from './Component/MoodRendering'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
