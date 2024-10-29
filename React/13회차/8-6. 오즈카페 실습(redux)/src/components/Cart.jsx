4e337816689d23d3683f324ce756522b03c4b5ad
hanhyochan <hcartist569@gmail.com>
2024-10-29 17:21:35 +0900

    10/29

--- React/13회차/8-6. 오즈카페 실습(redux)/src/components/Cart.jsx
+++ React/13회차/8-6. 오즈카페 실습(redux)/src/components/Cart.jsx
@@ -1,26 +1,33 @@
+import { useSelector } from "react-redux"
 import data from "../assets/data"
+import { removeFromCart } from "../redux/redux"
+import { useDispatch } from "react-redux"
 
-function Cart ({menu, cart, setCart}) {
-    if (!menu) return (<div style={{textAlign:"center", margin: '80px'}}> 메뉴 정보가 없어요!</div>)
+function Cart() {
+
+    const menu = useSelector(state => state.menuReducer)
+    const cart = useSelector(state => state.cartReducer)
+
+    if (!menu) return (<div style={{ textAlign: "center", margin: '80px' }}> 메뉴 정보가 없어요!</div>)
     const allMenus = [...menu.커피, ...menu.논커피]
     return (
         <>
             <h2>장바구니</h2>
             <ul className="cart">
-                {cart?.length ? cart.map(el => <CartItem 
-                    key={el.id} 
-                    item={allMenus.find(menu => menu.id === el.id)} 
-                    options={el.options} 
+                {cart?.length ? cart.map(el => <CartItem
+                    key={el.id}
+                    item={allMenus.find(menu => menu.id === el.id)}
+                    options={el.options}
                     quantity={el.quantity}
-                    cart={cart}
-                    setCart={setCart}
                 />) : <div className="no-item">장바구니에 담긴 상품이 없어요!</div>}
             </ul>
         </>
     )
 }
 
-function CartItem ({item, options, quantity, cart, setCart}) {
+
+function CartItem({ item, options, quantity }) {
+    const dispatch = useDispatch()
     return (
         <li className='cart-item'>
             <div className="cart-item-info">
@@ -32,7 +39,7 @@ function CartItem ({item, options, quantity, cart, setCart}) {
                 <div>개수 : {quantity}</div>
             </div>
             <button className="cart-item-delete" onClick={() => {
-                setCart( cart.filter(el => item.id !== el.id) )
+                dispatch(removeFromCart(item.id))
             }}>삭제</button>
         </li>
     )
