fbb9cb26ba3ee7e76533787f4f0e063d889b7a70
hanhyochan <hcartist569@gmail.com>
2024-11-19 21:46:49 +0900

    11/19

--- node, express/kakaoLogin/login.js
+++ node, express/kakaoLogin/login.js
@@ -6,7 +6,40 @@ const logoutButton = document.querySelector('#logout_button')
 
 const KAKAO_CLIENT_ID = '57c385c7f96f0e8ef99626fe0c4f16cf'
 const KAKAO_REDIRECT_URL = 'http://127.0.0.1:5500/kakaoLogin/index.html'
+let KAKAO_ACCESSTOKEN = ''
+
+const renderUserInfo = (imgUrl, name) => {
+    userImage.src = imgUrl
+    userName.textContent = name
+}
 
 kakaoButton.onclick = () => {
     location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`
+}
+
+window.onload = () => {
+    const url = new URL(location.href)
+    const urlParams = url.searchParams
+    const authorizationCode = urlParams.get('code')
+
+    axios.post('http://localhost:3000/kakao/login', { authorizationCode })
+        .then(res => {
+            KAKAO_ACCESSTOKEN = res.data
+            return axios.post('http://localhost:3000/kakao/userInfo', { KAKAO_ACCESSTOKEN })
+        })
+        .then(res => {
+            renderUserInfo(res.data.profile_image, res.data.nickname)
+        })
+}
+
+logoutButton.onclick = () => {
+    axios.delete('http://localhost:3000/kakao/logout', {
+        data: { KAKAO_ACCESSTOKEN }
+    })
+        .then(res => {
+            {
+                console.log(res.data)
+                renderUserInfo('', '')
+            }
+        })
 }
--- node, express/kakaoLogin/server.js
+++ node, express/kakaoLogin/server.js
@@ -5,12 +5,49 @@ const axios = require('axios')
 const app = express()
 
 app.use(cors({
-    origin: ['http://127.0.0.1:5500/kakaoLogin/index.html', 'http://localhost:5500/kakaoLogin/index.html'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
     methods: ['OPTIONS', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type']
 }))
 
+const KAKAO_CLIENT_ID = '57c385c7f96f0e8ef99626fe0c4f16cf'
+const KAKAO_REDIRECT_URL = 'http://127.0.0.1:5500/kakaoLogin/index.html'
+
 app.use(express.json())
+
+app.post('/kakao/login', (req, res) => {
+    const authorizationCode = req.body.authorizationCode
+    axios.post('https://kauth.kakao.com/oauth/token', {
+        grant_type: 'authorization_code',
+        client_id: KAKAO_CLIENT_ID,
+        redirect_uri: KAKAO_REDIRECT_URL,
+        code: authorizationCode
+    }, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+    })
+        .then(response => res.send(response.data.access_token))
+})
+
+app.post('/kakao/userinfo', (req, res) => {
+    const { KAKAO_ACCESSTOKEN } = req.body
+    axios.get('https://kapi.kakao.com/v2/user/me', {
+        headers: {
+            Authorization: `Bearer ${KAKAO_ACCESSTOKEN}`,
+            'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+    })
+        .then(response => res.json(response.data.properties))
+})
+
+app.delete('/kakao/logout', (req, res) => {
+    const { KAKAO_ACCESSTOKEN } = req.body
+    axios.post('https://kapi.kakao.com/v1/user/logout', {}, {
+        headers: { Authorization: `Bearer ${KAKAO_ACCESSTOKEN}` }
+    })
+        .then(response => res.send('로그아웃 성공'))
+})
 
 app.listen(3000, () => console.log('서버 열림!'))
 
