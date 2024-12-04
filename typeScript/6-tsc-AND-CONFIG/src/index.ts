14362e646a43eb4b3d5f941fb3a091b795ec6294
hanhyochan <hcartist569@gmail.com>
2024-12-04 15:15:11 +0900

    12/04

---
+++ typeScript/6-tsc-AND-CONFIG/src/index.ts
@@ -0,0 +1,11 @@
+// console.log("hello")
+
+// const nums: number[] = [];
+// const a = 10 + nums[0]
+
+// const someString = "typescript!";
+// someString.endwidth("!")
+
+import { printName } from "./util";
+
+printName("typescirpt")
